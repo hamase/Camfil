50a7feb5dadc4761e21444cec60e217ac4734d7d
Hamase Shahabbaspour <hamase86@gmail.com>
2020-11-17 19:53:43 +0330

    change footer and add home page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
 	$(".sidenav").sidenav();
 	$(".collapsible").collapsible();
+
+	$(".carousel.carousel-slider").carousel({
+		fullWidth: true,
+		indicators: true,
+	});
 });
