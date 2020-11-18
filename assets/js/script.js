1c6344722d5b4c504c426408910e3dade509e117
Hamase Shahabbaspour <hamase86@gmail.com>
2020-11-18 22:18:25 +0330

    add contact page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,21 @@ $(document).ready(function () {
 		fullWidth: true,
 		indicators: true,
 	});
+	$("select").formSelect();
+
+	// $("#contact-us input").on("keyup", function () {
+	// 	var validator = $("#contact-us").validate();
+	// 	if (validator.form() && validateEmail($("#email").val())) {
+	// 		$("#msg-btn").prop("disabled", false);
+	// 		$("#msg-btn").removeClass("disabled");
+	// 	} else {
+	// 		$("#msg-btn").prop("disabled", true);
+	// 		$("#msg-btn").addClass("disabled");
+	// 	}
+	// });
+
+	// function validateEmail(email) {
+	// 	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	// 	return re.test(email.toLowerCase());
+	// }
 });
