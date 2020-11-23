624251c9d48e824f6b1e9e50658a1baaa32e853e
Hamase Shahabbaspour <hamase86@gmail.com>
2020-11-23 17:07:51 +0330

    resets modal added

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,15 +18,47 @@ $(document).ready(function () {
 		$(this).closest(".modal").modal("close");
 		$("#sign-up").modal("open");
 	});
+	$("#sign-up-btn-2").click(function () {
+		$(this).closest(".modal").modal("close");
+		$("#sign-up").modal("open");
+	});
 	$("#forget-pass").click(function () {
 		$(this).closest(".modal").modal("close");
 		$("#forget").modal("open");
 	});
 
+	// ok modals
+	$("#register-submit").click(function () {
+		$(this).closest(".modal").modal("close");
+		$("#register-ok").modal("open");
+	});
+	$("#reset-pass-btn").click(function () {
+		// this is temporary, it will be shown by email link
+		$(this).closest(".modal").modal("close");
+		$("#reset-pass").modal("open");
+	});
+	$("#reset-pass-ok").click(function () {
+		$(this).closest(".modal").modal("close");
+		$("#reset-ok").modal("open");
+	});
+
+	// close just by close button, not click on body
 	$("#sign-up").modal({
 		dismissible: false,
 	});
 	$("#login").modal({
+		dismissible: false,
+	});
+	$("#forget").modal({
+		dismissible: false,
+	});
+	$("#register-ok").modal({
+		dismissible: false,
+	});
+	$("#reset-pass").modal({
+		dismissible: false,
+	});
+	$("#reset-ok").modal({
 		dismissible: false,
 	});
 
