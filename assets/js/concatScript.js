4aaf1f1e7c96594386247effa2824ee9435f7e43
Hamase Shahabbaspour <hamase86@gmail.com>
2020-12-16 16:01:10 +0330

    added debit card

--- assets/js/concatScript.js
+++ assets/js/concatScript.js
@@ -220,6 +220,15 @@ $(document).ready(function () {
 	$(".payment_box li.payment").on("click", function () {
 		$("li.payment").css("border-color", "#e2e2e2");
 		$(this).css({ "box-shadow": "none", "border-color": "#0086e6" });
-		$(".code_number").css("display", "block");
+	});
+	$("#debit").on("click", function () {
+		$(".paypal").css("display", "none");
+		$(".bank").css("display", "none");
+		$(".debit").css("display", "block");
+	});
+	$("#paypal").on("click", function () {
+		$(".debit").css("display", "none");
+		$(".bank").css("display", "none");
+		$(".paypal").css("display", "block");
 	});
 });
